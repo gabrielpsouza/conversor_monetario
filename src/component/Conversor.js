6e33c83a135eb4644bb4d3bcdbc0f9e5c206a83e
Gabriel Pedro Souza <g.pedro@kroton.com.br>
2020-02-20 17:32:26 -0300

    adicionando estilo na página

--- src/component/Conversor.js
+++ src/component/Conversor.js
@@ -17,15 +17,20 @@ export default class Conversor extends Component {
         let for_to = `${this.props.moedaA}_${this.props.moedaB}`;
         let url = `https://free.currconv.com/api/v7/convert?apiKey=do-not-use-this-key&q=${for_to}&compact=y`
 
-        fetch(url)
-        .then(res => {
-            return res.json()
-        })
-        .then(json => {
-            let cotacao = json[for_to].val;
-            let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
-            this.setState({ moedaB_valor });
-        })
+        if (this.state.moedaA_valor ===  ""  || this.state.moedaA_valor === undefined) {
+            alert("Nenhum valor informado!!");
+            this.state.moedaB_valor = 0;
+        } else {
+            fetch(url)
+            .then(res => {
+                return res.json()
+            })
+            .then(json => {
+                let cotacao = json[for_to].val;
+                let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
+                this.setState({ moedaB_valor });
+            });     
+        }
     }
     
     render() {
@@ -33,7 +38,7 @@ export default class Conversor extends Component {
             <div className="conversor">
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
                 <input type="text" onChange={(event)=> this.setState({moedaA_valor: event.target.value})} />
-                <input type="button" value="Converter" onClick={this.converter} />
+                <input type="button" className="btnConverter" value="Converter" onClick={this.converter} />
                 <h2>Valor convertido: {this.state.moedaB_valor}</h2>
             </div>
         );
